75450d06d5e32f1088b79d12afe69a3d071d4229
abhtri2009 <abhtri2009@gmail.com>
2017-07-22 17:09:58 +0530

    final code

--- StringReverse/src/com/abhtri2009/StringReverse.java
+++ StringReverse/src/com/abhtri2009/StringReverse.java
@@ -1,10 +1,29 @@
 package com.abhtri2009;
 
+import java.util.Scanner;
+
+import javax.swing.plaf.basic.BasicTabbedPaneUI;
+
 public class StringReverse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I love Anu :) ");
+		Scanner s = new Scanner(System.in);
+		String b = s.nextLine();
+		String r ="";
+		 r = reverse(b,r);
+		System.out.println(r);
+	}
+	
+	public static String reverse(String b,String r) {
+		if(b.length()>1) {
+			 r= reverse(b.substring(1, b.length()),r);
+			  return r=r+b.substring(0,1);
+		}
+		else {
+			r = r + b;
+			return r;
+		}
 	}
 
 }
